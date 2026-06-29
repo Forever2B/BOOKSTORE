83258ec8d3c2a8d58121612cae9fb2cab2ae6bdd
Forever2B <130312444+Forever2B@users.noreply.github.com>
2026-06-29 13:15:49 -0300

    wip: PythonAnywhere configuration
    edited settings.py to allow pythonAnywhere hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -170,4 +170,5 @@ DEBUG = os.environ.get("DEBUG", "1").lower() in ('true', '1', 'yes', 'on')
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split()
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]',
+                               'Forever2B.pythonanywhere.com').split()
