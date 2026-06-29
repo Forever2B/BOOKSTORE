d8e859d770666e15679b5cfe21aa534a00d74867
Forever2B <130312444+Forever2B@users.noreply.github.com>
2026-06-29 17:52:16 -0300

    correção de Allowed_hosts
    .

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -167,13 +167,9 @@ DEBUG = os.environ.get("DEBUG", "1").lower() in ('true', '1', 'yes', 'on')
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', '').strip()
-if allowed_hosts:
-    ALLOWED_HOSTS = allowed_hosts.split()
-else:
-    ALLOWED_HOSTS = [
-        'localhost',
-        '127.0.0.1',
-        '[::1]',
-        'forever2b.pythonanywhere.com',
-    ]
+
+ALLOWED_HOSTS = ['DJANGO_ALLOWED_HOSTS',
+                 'localhost',
+                 '127.0.0.1',
+                 '[::1]',
+                 'forever2b.pythonanywhere.com']
