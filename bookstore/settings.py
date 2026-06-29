8223d9ce5cfb38d53927f422cadbbd4096668637
Forever2B <130312444+Forever2B@users.noreply.github.com>
2026-06-29 17:43:54 -0300

    .
    .

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -167,8 +167,13 @@ DEBUG = os.environ.get("DEBUG", "1").lower() in ('true', '1', 'yes', 'on')
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-
-ALLOWED_HOSTS = os.environ.get(
-    'DJANGO_ALLOWED_HOSTS',
-    'localhost 127.0.0.1 [::1] forever2b.pythonanywhere.com'
-).split()
+allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', '').strip()
+if allowed_hosts:
+    ALLOWED_HOSTS = allowed_hosts.split()
+else:
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+        '[::1]',
+        'forever2b.pythonanywhere.com',
+    ]
