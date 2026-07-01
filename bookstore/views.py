462f3b8e883cd26649809f31c94cb6b87de6d8b3
Forever2B <130312444+Forever2B@users.noreply.github.com>
2026-07-01 13:04:09 -0300

    ODEIOPYTHONANYWHERE
    .

--- bookstore/views.py
+++ bookstore/views.py
@@ -19,8 +19,8 @@ def update(request):
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
     else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
-
+        return HttpResponse("Updated code on PythonAnywhere")
+        # return HttpResponse("Couldn't update the code on PythonAnywhere")
 
 def hello_world(request):
   template = loader.get_template('hello_world.html')
